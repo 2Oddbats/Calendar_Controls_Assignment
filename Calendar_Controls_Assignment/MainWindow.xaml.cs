6e4aabd78b6ba6c897a8886c859a8d36217fa03e
Isaiah Skelton <22wiskelton@macon.k12.mo.us>
2021-09-30 12:28:49 -0500

    I'm not sure exactly what happened, but calendar can be clicked now

---
+++ Calendar_Controls_Assignment/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Calendar_Controls_Assignment
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Get First Date
+
+        private void myCalendar_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
+        {
+            labelResult.Content = myCalendar.SelectedDate.ToString();
+
+
+
+        }
+
+    }
+}
